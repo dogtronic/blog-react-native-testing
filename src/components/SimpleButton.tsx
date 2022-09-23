4004b9b1d6b631983dea0aad26be3101c892e4a1
Michał Matuła <mmatula@concisesoftware.com>
2022-09-23 10:39:21 +0200

    configuration

---
+++ src/components/SimpleButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+type ButtonProps = {
+  label: string;
+  onPress: () => void;
+};
+
+const ButtonContainer = styled.TouchableOpacity`
+  height: ${56}px;
+  width: 100%;
+  margin-top: 17px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.background.dark};
+`;
+
+const ButtonText = styled.Text`
+font-family: "RobotoMono-Medium"
+font-size:16px
+color:
+ ${props => props.theme.colors.text.lightGrey}
+line-height:21px
+`;
+
+const SimpleButton: React.FC<ButtonProps> = ({label, onPress}) => {
+  return (
+    <ButtonContainer onPress={onPress}>
+      <ButtonText>{label}</ButtonText>
+    </ButtonContainer>
+  );
+};
+
+export default SimpleButton;
